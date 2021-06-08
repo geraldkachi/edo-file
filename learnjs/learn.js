02d165136833776dff227f1ca9013e433325fb26
geraldkachi <fitzgeraldkachi@gmail.com>
2021-06-08 16:10:17 +0100

    learn js from scratch

---
+++ learnjs/learn.js
@@ -0,0 +1,109 @@
+var product = 8 * 10
+console.log(product);
+
+// increment
+var myVar = 11
+myVar = myVar + 1
+
+// shortcut 
+myVar++
+
+
+// decrement 
+var myVar = 11
+myVar = myVar - 1
+
+// shortcut 
+myVar--
+
+// decimal can alos be called floats
+var outDecimal = 5.7
+
+var mux = 2.0 * 2.5
+console.log(mux);
+
+// compound Assignments with augmented Additions
+
+var a = 3
+var b = 17 
+var c = 12
+
+a = a + 12
+a += 12
+
+b = 9 + b
+b += 9
+
+c = c + 7
+c += 7
+
+// compound Assignments with augmented Substraction
+var a = 3
+var b = 17 
+var c = 12
+
+a = a - 12
+a -= 12
+
+b = 9 - b
+b -= 9
+
+c = c - 7
+c -= 7
+
+// compound Assignments with augmented Multiplication
+
+var a = 3
+var b = 17 
+var c = 12
+
+a = a * 12
+a *= 12
+
+b = 9 * b
+b *= 9
+
+c = c * 7
+c *= 7
+
+
+
+// compound Assignments with augmented Division
+var a = 3
+var b = 17 
+var c = 12
+
+a = a / 12
+a /= 12
+
+b = 9 / b
+b /= 9
+
+c = c / 7
+c /= 7
+
+//  STRING
+
+var myStr = "I am a \"double quoted\" string inside \" double quotes"
+console.log(myStr);
+
+// you can wirte template string anywhere. you can wirt esingle and double string inside a template string
+var myTemStr = '<a href="http://ww.exapmle.com" target="_blank">Link</a>'
+
+
+
+// Escape Sequences in Strings
+
+/**** 
+ CODE OUTPUT
+
+ \' single qoute
+ \"" doub;e quote
+ \\ backslash
+ \n newline
+ \r carriage return
+ \t tab
+ \b backspace
+ \f form feed
+
+ ****/
